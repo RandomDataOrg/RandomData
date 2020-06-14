7976a915970884faee176ecbd6dcb9e50ba84eaf
Krzysztof Gwóźdź <krzysztof.gwozdz@outlook.com>
2020-06-14 11:22:51 +0200

    Add simple random guid endpoint

---
+++ RandomData.Api/Controllers/GuidController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RandomData.Api.Controllers
+{
+	[ApiController]
+	[Route("/guid")]
+	public class GuidController
+	{
+		public async Task<string> GetRandomGuid()
+		{
+			return Guid.NewGuid().ToString();
+		}
+	}
+}
