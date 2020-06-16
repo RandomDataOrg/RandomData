a5b47d0edeff5f780e07c7170283a6c1a53531bc
Krzysztof Gwóźdź <krzysztof.gwozdz@outlook.com>
2020-06-16 23:45:26 +0200

    Add random guid parameters validation

---
+++ RandomData.Api.Tests/GuidGenerators/GetRandomGuidParametersValidatorTests.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using RandomData.Api.GuidGenerators;
+using System.Linq;
+using Xunit;
+
+namespace RandomData.Api.Tests.GuidGenerators
+{
+	public class GetRandomGuidParametersValidatorTests
+	{
+		[Fact]
+		public void DefaultParameters_Valid()
+		{
+			//arrange
+			var parameters = new GetRandomGuidParameters();
+			var validator = new GetRandomGuidParametersValidator();
+
+			//act
+			var validationResult = validator.Validate(parameters);
+
+			//assert
+			validationResult.IsValid.Should().BeTrue();
+		}
+
+		[Fact]
+		public void DefaultFormatCombinedWithOtherFormat_InvalidAndHasError()
+		{
+			//arrange
+			var parameters = new GetRandomGuidParameters
+			{
+				Formats = new[] { RandomGuidFormats.Default, RandomGuidFormats.Uppercase }
+			};
+			var validator = new GetRandomGuidParametersValidator();
+
+			//act
+			var validationResult = validator.Validate(parameters);
+
+			//assert
+			validationResult.IsValid.Should().BeFalse();
+			validationResult.Errors.Should().NotBeEmpty();
+			validationResult.Errors.First().ErrorCode.Should().Be(GetRandomGuidParametersValidator.ErrorCode);
+			validationResult.Errors.First().ErrorMessage.Should().Be($"{nameof(RandomGuidFormats.Default)} format cannot be combined with any other format.");
+		}
+	}
+}
--- RandomData.Api/GuidGenerators/GuidController.cs
+++ RandomData.Api/GuidGenerators/GuidController.cs
@@ -7,16 +7,21 @@ namespace RandomData.Api.GuidGenerators
 	public class GuidController : Controller
 	{
 		private readonly IGuidGenerator _generator;
+		private readonly IGetRandomGuidParametersValidator _validator;
 
-		public GuidController(IGuidGenerator generator)
+		public GuidController(IGuidGenerator generator, IGetRandomGuidParametersValidator validator)
 		{
 			_generator = generator;
+			_validator = validator;
 		}
 
 		[HttpGet]
 		[Route("")]
 		public ActionResult<string> GetRandomGuid([FromQuery] GetRandomGuidParameters parameters)
 		{
+			var validationResult = _validator.Validate(parameters);
+			if (!validationResult.IsValid)
+				return BadRequest(validationResult.Errors);
 			return Ok(_generator.GetRandom(parameters));
 		}
 	}
