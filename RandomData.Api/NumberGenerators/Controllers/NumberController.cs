bf965335ff356e098bfbf4db65ca35cc8f064011
ntiany <krolkas@gmail.com>
2020-06-22 09:26:04 +0200

    Add number query

---
+++ RandomData.Api/NumberGenerators/Controllers/NumberController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RandomData.Api.NumberGenerators.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NumberController : Controller
+    {
+        [System.Web.Http.HttpGet]
+        public ActionResult<string> GetRandomNumber([FromQuery] string parameters)
+        {
+            return Ok("");
+        }
+    }
+}
---
+++ RandomData.Api/NumberGenerators/Dto/NumberParameters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RandomData.Api.NumberGenerators.Dto
+{
+    public class NumberParameters
+    {
+        private const string DefaultAllowedDigits = "123456789";
+
+        [FromQuery(Name = "minLength")] public int MinLength { get; set; } = 1;
+
+        [FromQuery(Name = "maxLength")] public int MaxLength { get; set; } = 5;
+
+        [FromQuery(Name = "allowedDigits")] public string AllowedDigits { get; set; } = DefaultAllowedDigits;
+    }
+}
