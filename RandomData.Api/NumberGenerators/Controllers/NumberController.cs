b6afafac2ab33074f023d72232509d8ce95840f6
ntiany <krolkas@gmail.com>
2020-06-22 11:50:08 +0200

    Add returning random number from API

--- RandomData.Api/NumberGenerators/Controllers/NumberController.cs
+++ RandomData.Api/NumberGenerators/Controllers/NumberController.cs
@@ -1,15 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RandomData.Api.NumberGenerators.Dto;
+using RandomData.Api.NumberGenerators.Generators;
+using RandomData.Api.NumberGenerators.Validator;
 
 namespace RandomData.Api.NumberGenerators.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class NumberController : Controller
+    public class NumberController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult<string> GetRandomNumber([FromQuery] string parameters)
+        private readonly NumberGenerator _generator;
+
+        private readonly NumberParametersValidator _validator;
+
+        public NumberController()
         {
-            return Ok("");
+            _generator = new NumberGenerator();
+            _validator = new NumberParametersValidator();
+        }
+
+        [HttpGet]
+        public ActionResult<string> GetRandomNumber([FromQuery] NumberParameters parameters)
+        {
+            var validationResult = _validator.Validate(parameters);
+            if (!validationResult.IsValid)
+            {
+                return BadRequest(validationResult.Errors);
+            }
+            return Ok(_generator.GetRandom(parameters));
         }
     }
 }
---
+++ RandomData.Api/NumberGenerators/Generators/NumberGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RandomData.Api.NumberGenerators.Dto;
+
+namespace RandomData.Api.NumberGenerators.Generators
+{
+    public class NumberGenerator
+    {
+        private readonly Random _random;
+
+        public NumberGenerator()
+        {
+            _random = new Random();
+        }
+
+        public string GetRandom(NumberParameters parameters)
+        {
+            var length = _random.Next(parameters.MinLength, parameters.MaxLength + 1);
+
+            var result = new string(Enumerable.Repeat(parameters.AllowedDigits, length)
+                .Select(d => d[_random.Next(d.Length)]).ToArray());
+
+            return result;
+        }
+    }
+}
