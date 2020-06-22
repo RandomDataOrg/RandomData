faf096864c5f703cb7c64c0d287898ec8e6e3e51
ntiany <krolkas@gmail.com>
2020-06-22 09:33:45 +0200

    Add validator

---
+++ RandomData.Api/NumberGenerators/Validator/NumberParametersValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using FluentValidation;
+using RandomData.Api.NumberGenerators.Dto;
+
+namespace RandomData.Api.NumberGenerators.Validator
+{
+    public class NumberParametersValidator : AbstractValidator<NumberParameters>
+    {
+        public NumberParametersValidator()
+        {
+            RuleFor(x => x.MinLength).GreaterThanOrEqualTo(1);
+            RuleFor(x => x.MaxLength).LessThanOrEqualTo(int.MaxValue);
+            RuleFor(x => x.MaxLength).GreaterThanOrEqualTo(x => x.MinLength);
+            RuleFor(x => x.AllowedDigits).MinimumLength(1);
+        }
+    }
+}
