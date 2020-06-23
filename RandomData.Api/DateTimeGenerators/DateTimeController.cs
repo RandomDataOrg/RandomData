debd216354deeb1326ee8e0c266e0cc94089849a
Mateusz Golombniak <Mejtika@hotmail.com>
2020-06-23 10:50:00 +0200

    Add random datetime endpoint

---
+++ RandomData.Api/DateTimeGenerators/DateTimeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RandomData.Api.DateTimeGenerators
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class DateTimeController : ControllerBase
+    {
+		private readonly DateTimeGenerator _dateTimeGenerator;
+
+		public DateTimeController(DateTimeGenerator dateTimeGenerator)
+		{
+			_dateTimeGenerator = dateTimeGenerator;
+		}
+
+		/// <summary>
+		/// Returns random DateTime between min and max dates.
+		/// </summary>
+		[HttpGet]
+		public IActionResult GetRandomDateTime([FromQuery] RandomDateTimeParameters request)
+		{
+			return Ok(_dateTimeGenerator.Generate(request));
+		}
+	}
+}
