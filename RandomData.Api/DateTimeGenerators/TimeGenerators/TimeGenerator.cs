f393e00c8c78c0630baf22da425a6a83d3620ea6
Mateusz Golombniak <Mejtika@hotmail.com>
2020-06-23 12:07:37 +0200

    Add random time generator

---
+++ RandomData.Api/DateTimeGenerators/TimeGenerators/TimeGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace RandomData.Api.DateTimeGenerators.TimeGenerators
+{
+    public class TimeGenerator
+    {
+        private readonly DateTimeGenerator _dateTimeGenerator;
+
+        public TimeGenerator(DateTimeGenerator dateTimeGenerator)
+        {
+            _dateTimeGenerator = dateTimeGenerator;
+        }
+
+        public string Generate(RandomTimeParameters parameters)
+        {
+            var min = Convert.ToDateTime(parameters.MinTime);
+            var max = Convert.ToDateTime(parameters.MaxTime);
+            var format = GetFormatedTime(parameters.Format);
+
+            var dateTimeParameters = new RandomDateTimeParameters
+            {
+                MinDate = min,
+                MaxDate = max,
+                Format = format
+            };
+
+            return _dateTimeGenerator.Generate(dateTimeParameters);
+        }
+
+        private string GetFormatedTime(RandomTimeFormats format) =>
+            format switch
+            {
+                RandomTimeFormats.Short => "t",
+                RandomTimeFormats.Long => "T",
+                _ => throw new ArgumentOutOfRangeException(nameof(format), format, null)
+            };
+    }
+}
